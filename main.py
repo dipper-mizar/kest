bf9af6b2729b03c33b2a0b2524079ff0f044d83e
dipper-mizar <1923001710@qq.com>
2021-10-07 00:23:34 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from webob import Request, Response
+from webob.dec import wsgify
+from paste.deploy import loadapp
+
+from wsgiref.simple_server import make_server
+
+import os
+
+
+# @wsgify
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'HELLO WORLD!']
+    # return Response('hello world!\n')
+
+
+def app_factory(global_config, **local_config):
+    return application
+
+
+if __name__ == '__main__':
+    configname = 'paste.ini'
+    appname = 'main'
+
+    print('listen on 8000...')
+
+    wsgi_app = loadapp('config:%s' % os.path.abspath(configname), appname)
+
+    httpd = make_server('127.0.0.1', 8000, wsgi_app)
+    httpd.serve_forever()
